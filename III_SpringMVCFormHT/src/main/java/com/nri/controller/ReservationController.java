49420c603b166f20feb45429f33e9465eade53a5
manshiginodia <manshiginodia@gmail.com>
2022-11-23 16:39:53 +0530

    23/11

--- III_SpringMVCFormHT/src/main/java/com/nri/controller/ReservationController.java
+++ III_SpringMVCFormHT/src/main/java/com/nri/controller/ReservationController.java
@@ -1,5 +1,6 @@
 package com.nri.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
@@ -14,13 +15,15 @@ import com.nri.model.Reservation;
 //@RequestMapping(value ="/ticket")
 public class ReservationController {
 
+	@Autowired
 	ReservationDaoImpl rdaoobj;
-	
-	
-	public ApplicationContext getIoC() {
-		
-		ApplicationContext ioc = new ClassPathXmlApplicationContext("nrispring-servlet.xml");
-		return ioc;
+
+	public ReservationDaoImpl getRdaoobj() {
+		return rdaoobj;
+	}
+
+	public void setRdaoobj(ReservationDaoImpl rdaoobj) {
+		this.rdaoobj = rdaoobj;
 	}
 
 	@RequestMapping(value = "/bookingForm")
@@ -33,13 +36,48 @@ public class ReservationController {
 	}
 
 	// insert a record
-	@RequestMapping(value ="/submitForm")
+	@RequestMapping(value = "/submitForm")
 	public String submitForm(@ModelAttribute("reservation") Reservation reservation) {
-		rdaoobj= (ReservationDaoImpl)getIoC().getBean("reservDao");
 		rdaoobj.rcrInsert(reservation);
-		
-		
+
 		return "confirmation";
 	}
 
 }
+
+
+
+/*
+We can capture the checked values in controller and store in pojo/model class as a object and 
+finally store in to the database using hibernate.
+
+The primary key attribute with the @GeneratedValue annotation and set GenerationType.SEQUENCE as 
+the strategy. This tells Hibernate to use a database sequence to generate the primary key value. 
+If you don’t provide any additional information, Hibernate will use its default sequence.
+
+You can configure the name and schema of the database sequence with a @SequenceGenerator 
+annotation like the one you can see in the following code snippet.
+
+@Entity
+public class Reservation {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "author_generator")
+    @SequenceGenerator(name="author_generator", sequenceName = "author_seq")
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+     
+    ...
+     
+}
+*/
+
+
+
+
+
+
+
+
+
+
--- III_SpringMVCFormHT/src/main/java/com/nri/dao/ReservationDaoImpl.java
+++ III_SpringMVCFormHT/src/main/java/com/nri/dao/ReservationDaoImpl.java
@@ -6,9 +6,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
 
 import com.nri.model.Reservation;
 
+@Repository
 public class ReservationDaoImpl implements ReservationDao{
 
 	@Autowired
